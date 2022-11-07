b9a8bc7742fb8cbedb83a2e6d606528663167142
cppizarro <cppizarro@miuandes.cl>
2022-11-07 00:56:00 -0300

    settings

--- tb_pds/settings.py
+++ tb_pds/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import django_on_heroku
+# import django_on_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,4 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
 
-django_on_heroku.settings(locals())
+# django_on_heroku.settings(locals())
+# django_heroku.settings(locals())
