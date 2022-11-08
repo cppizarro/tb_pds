ed3ff7f27272c406b24dd965c3e8aa1917dc8134
cppizarro <cppizarro@miuandes.cl>
2022-11-07 21:43:03 -0300

    heroku?

--- tb_pds/settings.py
+++ tb_pds/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=MAX_CONN_AGE, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=MAX_CONN_AGE, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
